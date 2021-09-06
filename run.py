8843a4f319832327c27648c473b0966033c884b6
949027 <82878668+949027@users.noreply.github.com>
2021-09-06 17:19:28 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,21 +1,21 @@
-import cv2
+import cv2 #подключение библиотеки "cv2"
 
-from config import CASCADES
-
-
-def is_user_wants_quit():
-    return cv2.waitKey(1) & 0xFF == ord('q')
+from config import CASCADES # импорт каскадов
 
 
-def show_frame(frame):
+def is_user_wants_quit(): # функция для выхода
+    return cv2.waitKey(1) & 0xFF == ord('q') #проверка нажатия клавиши "q"
+
+
+def show_frame(frame): # показать кадр
     cv2.imshow('Video', frame)
 
 
-def draw_sqare(frame, color):
+def draw_sqare(frame, color): # нарисовать рамку по координатам
         cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
 
 
-def get_cascades():
+def get_cascades(): # генератор списка каскадов
     cascades = [
         (cv2.CascadeClassifier(cascade['path']), cascade['color'])
         for title, cascade in CASCADES.items()
@@ -25,14 +25,14 @@ def get_cascades():
 
 
 if __name__ == "__main__":
-    cascades = get_cascades()
-    video_capture = cv2.VideoCapture(0)
-    while True:
-        if not video_capture.isOpened():
+    cascades = get_cascades() #запись каскадов в переменную
+    video_capture = cv2.VideoCapture(0) #захват изображения
+    while True: # выполнять цикл пока возможен захват изображения и пользователь не нажал кнопку для выхода
+        if not video_capture.isOpened(): # проверка возможности захвата изображения
             print("Couldn't find your webcam... Sorry :c")
-        _, webcam_frame = video_capture.read()
-        gray_frame = cv2.cvtColor(webcam_frame, cv2.COLOR_BGR2GRAY)
-        for cascade, color in cascades:
+        _, webcam_frame = video_capture.read() # запись кадра в переменную
+        gray_frame = cv2.cvtColor(webcam_frame, cv2.COLOR_BGR2GRAY) # преобразование кадра в Ч/Б
+        for cascade, color in cascades: #поиск совпадений между каскадами и кадром с камеры с помощью вложенных циклов
             captures = [cascade.detectMultiScale(
                 gray_frame,
                 scaleFactor=1.1,
@@ -41,10 +41,10 @@ if __name__ == "__main__":
             )]
             for capture in captures:
                 for (x, y, w, h) in capture:
-                    draw_sqare(webcam_frame, color)
-        show_frame(webcam_frame)
+                    draw_sqare(webcam_frame, color) #на каждом анализируемом объекте рисуется рамка
+        show_frame(webcam_frame) #отображение текущего кадра
 
-        if is_user_wants_quit():
+        if is_user_wants_quit(): #проверка нажатия кнопки пользователем для выхода
             break
-    video_capture.release()
-    cv2.destroyAllWindows()
+    video_capture.release() #вывод на экран
+    cv2.destroyAllWindows() #закрыть окна
